4da2f5b42ba432f89fd0639e9f06a4d570a8bb55
Rishabh Agarwal <img_2022055@iiitm.ac.in>
2025-01-25 22:40:46 +0530

    Fixed: Users have a hobby only once

--- generateUsers.js
+++ generateUsers.js
@@ -10,19 +10,25 @@ const predefinedHobbies = [
     'cricket', 'football', 'basketball', 'badminton',
     'swimming', 'squash', 'table-tennis', 'golf'
 ];
+
 const generateRandomUsers = async () => {
     const locations = ["Bangalore", "Pune", "Hyderabad", "Gurgaon", "Chennai", "Mumbai", "Kolkata", "Delhi"];
-    const users = Array.from({ length: 100 }, () => ({
-        fullName: `${faker.person.firstName()} ${faker.person.lastName()}`,
-        location: locations[Math.floor(Math.random() * locations.length)],
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-        salt: faker.string.alphanumeric(10),
-        hobbies: Array.from(
-            { length: faker.number.int({ min: 1, max: 5 }) }, 
-            () => predefinedHobbies[Math.floor(Math.random() * predefinedHobbies.length)] 
-        ),
-    }));
+    const users = Array.from({ length: 100 }, () => {
+        const hobbiesSet = new Set();
+        const hobbiesCount = faker.number.int({ min: 1, max: 5 });
+        while (hobbiesSet.size < hobbiesCount) {
+            const hobby = predefinedHobbies[Math.floor(Math.random() * predefinedHobbies.length)];
+            hobbiesSet.add(hobby);
+        }
+        return {
+            fullName: `${faker.person.firstName()} ${faker.person.lastName()}`,
+            location: locations[Math.floor(Math.random() * locations.length)],
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+            salt: faker.string.alphanumeric(10),
+            hobbies: Array.from(hobbiesSet), 
+        };
+    });
 
     await User.insertMany(users);
     console.log('Random users generated successfully!');
