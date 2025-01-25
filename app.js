972d7dfde4bf407eac73c50e6a283120ee2eb9f8
Rishabh Agarwal <img_2022055@iiitm.ac.in>
2025-01-25 23:49:56 +0530

    Added basic CRUD without authentication

--- app.js
+++ app.js
@@ -2,52 +2,74 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
-require('dotenv').config();
+require("dotenv").config();
 const User = require("./models/user.js");
 const methodOverride = require("method-override");
 
-mongoose.connect(process.env.MONGO_URL)
-    .then(() => console.log("MongoDB connected!"));
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("MongoDB connected!"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
+// Home Route
 app.get("/", (req, res) => {
     res.render("home");
 });
 
+// Users Index Route
 app.get("/users", async (req, res) => {
     const users = await User.find({});
-    res.render('users/index', { users });
+    res.render("users/index", { users });
 });
 
-app.get('/users/new', (req, res) => {
-    res.render('users/new');
+// New User Form Route
+app.get("/users/new", (req, res) => {
+    res.render("users/new");
 });
 
+// Create New User
 app.post("/users", async (req, res) => {
     const user = new User(req.body.user);
     await user.save();
     res.redirect(`/users/${user._id}`); // Redirect correctly
 });
 
+// Show User Route
 app.get("/users/:id", async (req, res) => {
     const user = await User.findById(req.params.id);
-    res.render('users/show', { user });
+    if (!user) {
+        return res.status(404).send("User not found");
+    }
+    res.render("users/show", { user });
 });
 
-app.get('/users/:id/edit', async (req, res) => {
+// Edit User Form Route
+app.get("/users/:id/edit", async (req, res) => {
     const user = await User.findById(req.params.id);
-    res.render('users/edit', { user });
+    if (!user) {
+        return res.status(404).send("User not found");
+    }
+    res.render("users/edit", { user });
 });
 
+// Update User
 app.put("/users/:id", async (req, res) => {
     const { id } = req.params;
     const user = await User.findByIdAndUpdate(id, { ...req.body.user });
-    res.redirect(`/users/${user._id}`); // Fix redirection to ensure only one `/users/` is present
+    res.redirect(`/users/${id}`); // Use `/users/${id}` directly
+});
+
+// Delete User
+app.delete("/users/:id", async (req, res) => {
+    const { id } = req.params;
+    await User.findByIdAndDelete(id);
+    res.redirect("/users");
 });
 
 app.listen(5000, () => {
